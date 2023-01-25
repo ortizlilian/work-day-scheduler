ec91714e0a3bc06358da217ed0856dfa6d6f0d40
Lily Dell <lily.starleks@gmail.com>
2023-01-25 17:29:46 +0000

    Added functions to use local storage
    - Created on click function that captures inputs and save them to local storage
    - Created for each loop to retrieve the item from local storage and populate the work scheduler

--- index.js
+++ index.js
@@ -24,41 +24,12 @@ timeblocks.each(function(index, value) {
     }
 });
 
+// Gets input and ID and saves it to local storage
+$("button").on("click", function() {
+    localStorage.setItem($(this).attr("data-buttonform"), $("#" + $(this).attr("data-buttonform")).val());
+});
 
-// 3. Save input to local storage
-//      create variable (an array) called inputs that will store all of the input data
-//      Add event listener to all save button
-//          Add event.preventDefault inside the click event listener
-//          Push the input value to inputs array with the format of { time: xx, input: xxx }
-//              Get the input value
-//              Get the hour value
-//              Push to the inputs array if the hour entry not exist yet in the array
-//              Replace the existing entry if the hour entry exist in the array
-//          Save inputs variable to local storage
-//              Stringify the inputs array
-//          Show feedback message to the user (optional)
-//          The feedback need to be dissappeared automatically
-//
-//      Example of inputs array format
-//      var inputs = [{
-//        time: 9,
-//        input: 'Meeting',
-//      },{
-//        time: 10,
-//        input: 'Coffee',
-//      },...];
-// 4. Load input from local storage when page load/refresh if there's any data in local storage
-//      var localStorageInput = get data from local storage
-//      check if localStorageInput exist, if it is
-//          var parsedLocalStorageInput = parse localStorageInput
-//          populate the inputs with the value from parsedLocalStorageInput
-//          inputs.forEach(function(input) {
-//              input => {
-//                time: 9,
-//                input: 'Meeting',
-//              }
-//              $('.timeblock[data-hour="' + input.time + '"] textarea').val(input.input)
-//          })
-//      if theres no data in local storage, do nothing
-
-// Extra, add hover effect on the save button
+// Retrieves input and ID from local storage
+$("textarea.form-control").each(function(index, value) {
+    $(this).text(localStorage.getItem($(this).attr("id")));
+});
